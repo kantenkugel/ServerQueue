f1b8937af6ecdd7c0f6932bf7a5843cb3d5cb5cf
Michael Ritter <ritter.michael92@gmail.com>
2012-02-09 18:05:00 +0100

    removing dummy permission and adding basic comments

--- src/me/kantenkugel/serveress/serverqueue/SQListener.java
+++ src/me/kantenkugel/serveress/serverqueue/SQListener.java
@@ -8,7 +8,7 @@ import org.bukkit.event.player.PlayerLoginEvent.Result;
 
 public class SQListener implements Listener {
 
-	private ServerQueue sqplugin;
+	private ServerQueue sqplugin;													//ServerQueue instance
 	
 	public SQListener(ServerQueue plugin) {
 		this.sqplugin = plugin;
@@ -16,44 +16,50 @@ public class SQListener implements Listener {
 	
 	@EventHandler
 	public void PlayerLogin(PlayerLoginEvent event) {
-		Player player = event.getPlayer();
-		if(this.sqplugin.getServer().getOnlinePlayers().length >= this.sqplugin.normalslots) {
+		Player player = event.getPlayer();											//get joining player
+		if(this.sqplugin.getServer().getOnlinePlayers().length >= this.sqplugin.normalslots) {//all normal slots used?
+			//check if vip and allowed to join
 			if((player.hasPermission("serverqueue.vip") && this.sqplugin.reserveperm == true) || player.hasPermission("serverqueue.master") || this.sqplugin.vipplayers.contains((String) player.getName())) {
+				//if all slots are used, try to kick
 				if(this.sqplugin.getServer().getOnlinePlayers().length == this.sqplugin.maxplayers) {
-					kicker(player, event);
+					kicker(player, event);											//seperate function
 				}
-			} else {
+				//else show them default server full message
+			} else {																//if no vip, dont allow connection
 				event.disallow(Result.KICK_FULL, "Server is full. If there are remaining slots, they are reserved for VIPs.");
 			}
 		}
 	}
 	
-	private void kicker(Player pl, PlayerLoginEvent event) {
+	private void kicker(Player pl, PlayerLoginEvent event) {						//kicks players for vips
+		//first check if the joining player is allowed to kick others
 		if(((pl.hasPermission("serverqueue.master") || this.sqplugin.vipplayers.contains((String) pl.getName())) && this.sqplugin.kickvip) || (pl.hasPermission("serverqueue.vip") && this.sqplugin.kickperm)) {
-			if(this.sqplugin.kicknotice == true) {
-				if(!(this.sqplugin.joinhash.containsKey(pl.getName()))) {
+		
+			if(this.sqplugin.kicknotice == true) {									//if notice is enabled
+				if(!(this.sqplugin.joinhash.containsKey(pl.getName()))) {			//not in hashmap, registering with current timestamp
 					this.sqplugin.joinhash.put(pl.getName(), System.currentTimeMillis());
 					event.disallow(Result.KICK_FULL, "Server is full. if you Join within the next 20 seconds, a player will get kicked.");
-				} else if((System.currentTimeMillis() - this.sqplugin.joinhash.get(pl.getName())) > 1000 * 20) {
+				} else if((System.currentTimeMillis() - this.sqplugin.joinhash.get(pl.getName())) > 1000 * 20) {//waited to long... registering again
 					this.sqplugin.joinhash.remove(pl.getName());
 					this.sqplugin.joinhash.put(pl.getName(), System.currentTimeMillis());
 					event.disallow(Result.KICK_FULL, "Server is full. if you Join within the next 20 seconds, a player will get kicked.");
-				} else {
+				} else {																						//kicking other player
 					this.sqplugin.joinhash.remove(pl.getName());
 					for(Player online: this.sqplugin.getServer().getOnlinePlayers()) {
+						//check if the target player is vip or has the permission serverqueue.nokick
 						if(!(online.hasPermission("serverqueue.nokick") || online.hasPermission("serverqueue.master") || online.hasPermission("serverqueue.vip") || this.sqplugin.vipplayers.contains(online.getName()))) {
 							this.sqplugin.logger.info("["+this.sqplugin.pdf.getName()+"] Kicking player "+online.getName()+" for a VIP.");
 							online.kickPlayer("You have been kicked for a VIP");
 							break;
 						}
 					}
-					if(this.sqplugin.getServer().getOnlinePlayers().length == this.sqplugin.maxplayers) {
+					if(this.sqplugin.getServer().getOnlinePlayers().length == this.sqplugin.maxplayers) {		//no player was kicked... notify joining player
 						event.disallow(Result.KICK_FULL, "No kickable Player Found... please try again later");
-					} else {
+					} else {																					//slot opened... allowing join
 						event.allow();
 					}
 				}
-			} else {
+			} else {																							//no notify... just kick
 				for(Player online: this.sqplugin.getServer().getOnlinePlayers()) {
 					if(!(online.hasPermission("serverqueue.nokick") || online.hasPermission("serverqueue.master") || online.hasPermission("serverqueue.vip") || this.sqplugin.vipplayers.contains(online.getName()))) {
 						this.sqplugin.logger.info("["+this.sqplugin.pdf.getName()+"] Kicking player "+online.getName()+" for a VIP.");
@@ -67,8 +73,6 @@ public class SQListener implements Listener {
 					event.allow();
 				}
 			}
-		} else {
-			event.disallow(Result.KICK_FULL, "The Server is currently full. Please try again soon");
 		}
 	}
 }
--- src/me/kantenkugel/serveress/serverqueue/ServerQueue.java
+++ src/me/kantenkugel/serveress/serverqueue/ServerQueue.java
@@ -14,22 +14,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerQueue extends JavaPlugin {
 
+	//Defining Variables
 	int maxplayers, normalslots, reservedslots;
 	boolean reserveperm, kickperm, kickvip, kicknotice;
-	List<Object> vipplayers;
-	HashMap<String, Long> joinhash = new HashMap<String, Long>();
+	List<Object> vipplayers;												//List with the VIPs from the config
+	HashMap<String, Long> joinhash = new HashMap<String, Long>();			//if Notify is enabled, this stores the time of last attempt
 	public final Logger logger = Logger.getLogger("Minecraft");
 	PluginDescriptionFile pdf;
 	
 	public void onEnable() {
 		this.pdf = this.getDescription();
 		PluginManager pm = getServer().getPluginManager();
-		this.maxplayers = getServer().getMaxPlayers();
-		this.getConfig().options().copyDefaults(true);
+		this.maxplayers = getServer().getMaxPlayers();						//get the slots of the server
+		this.getConfig().options().copyDefaults(true);						//Write default values to config, if section is not present
 		this.saveDefaultConfig();
-		this.saveConfig();
-		loadconfigfile();
-		pm.registerEvents(new SQListener(this), this);
+		this.saveConfig();													
+		loadconfigfile();													//saves the config-values to the java-values
+		pm.registerEvents(new SQListener(this), this);						//register the EventListener
+		//Print Status to console
 		this.logger.info("[" + this.pdf.getName() + "] There are " + this.getServer().getOnlinePlayers().length + "/" + this.maxplayers + "(" + this.normalslots + "+" + this.reservedslots + ") Players online.");
 		this.logger.info("[" + this.pdf.getName() + "] v" + this.pdf.getVersion() + " is enabled!");
 	}
@@ -38,29 +40,30 @@ public class ServerQueue extends JavaPlugin {
 		this.logger.info("[" + this.pdf.getName() + "] is disabled!");
 	}
 	
-	private void loadconfigfile() {
-		this.reloadConfig();
-		this.reservedslots = this.getConfig().getInt("ServerQueue.SlotsToReserve");
-		this.normalslots = this.maxplayers - this.reservedslots;
-		this.reserveperm = this.getConfig().getBoolean("ServerQueue.PermReserve");
-		this.kickperm = this.getConfig().getBoolean("ServerQueue.Kick.Perm");
-		this.kickvip = this.getConfig().getBoolean("ServerQueue.Kick.VIP");
-		this.kicknotice = this.getConfig().getBoolean("ServerQueue.Kick.Notice");
-		this.vipplayers = this.getConfig().getList("VIPs");
+	private void loadconfigfile() {											//reads the config-file and saves it to local variables
+		this.reloadConfig();												//if admin made changes, refresh the file
+		this.reservedslots = this.getConfig().getInt("ServerQueue.SlotsToReserve");		//slots to reserve
+		this.normalslots = this.maxplayers - this.reservedslots;						//calculate the normal slots (max-vip)
+		this.reserveperm = this.getConfig().getBoolean("ServerQueue.PermReserve");		//whether to reserve or not to reserve vipslots for permissionusers
+		this.kickperm = this.getConfig().getBoolean("ServerQueue.Kick.Perm");			//kick vor vip-permission
+		this.kickvip = this.getConfig().getBoolean("ServerQueue.Kick.VIP");				//kick vor vips in the config
+		this.kicknotice = this.getConfig().getBoolean("ServerQueue.Kick.Notice");		//notice joining vips before kicking
+		this.vipplayers = this.getConfig().getList("VIPs");								//load the vips from the file
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args) {
-		if(cmdlabel.equalsIgnoreCase("sq") || cmdlabel.equalsIgnoreCase("serverqueue")) {
-			if(sender != this.getServer().getConsoleSender() && !(sender.hasPermission("serverqueue.cmd"))) return noperm((Player) sender);
+		if(cmdlabel.equalsIgnoreCase("sq") || cmdlabel.equalsIgnoreCase("serverqueue")) {	//just to be sure
+			//check if player is allowed to use commands
+			if(sender != this.getServer().getConsoleSender() && !(sender.hasPermission("serverqueue.reload") || sender.hasPermission("serverqueue.status"))) return noperm((Player) sender);
 			if(args.length == 1) {
-				switch(args[0]) {
-				case "reload":
+				switch(args[0]) {															//switch between subcommands
+				case "reload":																//reloads the config-file (function loadconfig())
 					if(sender != this.getServer().getConsoleSender() && !(sender.hasPermission("serverqueue.reload"))) return noperm((Player) sender);
 					this.logger.info("["+this.pdf.getName()+"] Reloading Config-File!");
 					if(sender != this.getServer().getConsoleSender()) sender.sendMessage("["+this.pdf.getName()+"] Reloading Config-File!");
 					loadconfigfile();
 					break;
-				case "status":
+				case "status":																//prints the status-message to the player or console
 					if(sender != this.getServer().getConsoleSender() && !(sender.hasPermission("serverqueue.status"))) return noperm((Player) sender);
 					if(sender == this.getServer().getConsoleSender()) this.logger.info("["+this.pdf.getName()+"] Status: Players online: "+this.getServer().getOnlinePlayers().length+"/"+this.maxplayers+"("+(this.maxplayers - this.reservedslots)+"+"+this.reservedslots+")");
 					else sender.sendMessage("["+this.pdf.getName()+"] Status: Players online: "+this.getServer().getOnlinePlayers().length+"/"+this.maxplayers+"("+(this.maxplayers - this.reservedslots)+"+"+this.reservedslots+")");
@@ -75,7 +78,7 @@ public class ServerQueue extends JavaPlugin {
 		} else return false;
 	}
 	
-	private boolean noperm(Player pl) {
+	private boolean noperm(Player pl) {														//simple function to print a permission-error
 		pl.sendMessage(ChatColor.RED+"Your not allowed to do that");
 		return true;
 	}
